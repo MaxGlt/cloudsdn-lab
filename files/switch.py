552d610276a9e581d2dbfd28e8f6de937e8cb0c8
MaxGlt <mgaul470@gmail.com>
2025-05-26 11:44:02 +0200

    updating ryu playbook

--- files/switch.py
+++ files/switch.py
@@ -3,27 +3,54 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
+from ryu.lib.packet import packet, ethernet
 
-class L2Switch(app_manager.RyuApp):
+class LearningSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(L2Switch, self).__init__(*args, **kwargs)
+        super(LearningSwitch, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
+    def _packet_in_handler(self, ev):
         msg = ev.msg
         dp = msg.datapath
         ofp = dp.ofproto
-        ofp_parser = dp.ofproto_parser
+        parser = dp.ofproto_parser
+        in_port = msg.in_port
 
-        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
 
-        data = None
-        if msg.buffer_id == ofp.OFP_NO_BUFFER:
-             data = msg.data
+        dst = eth.dst
+        src = eth.src
+        dpid = dp.id
+        self.mac_to_port.setdefault(dpid, {})
 
-        out = ofp_parser.OFPPacketOut(
-            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions, data = data)
+        # Recording source MAC address
+        self.mac_to_port[dpid][src] = in_port
+
+        # Determine output port
+        out_port = self.mac_to_port[dpid].get(dst, ofp.OFPP_FLOOD)
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # If destination is known, install a flow rule
+        if out_port != ofp.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, dl_dst=dst)
+            dp.send_msg(parser.OFPFlowMod(
+                datapath=dp,
+                match=match,
+                priority=1,
+                actions=actions
+            ))
+
+        # Immediate response to received packet
+        out = parser.OFPPacketOut(
+            datapath=dp,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data if msg.buffer_id == ofp.OFP_NO_BUFFER else None
+        )
         dp.send_msg(out)
